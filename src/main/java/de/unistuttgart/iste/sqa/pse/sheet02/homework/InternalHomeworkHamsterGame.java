443e95442a616f5a3a163cb5d6753cf82fea9779
franksn90 <s.frankausowen@googlemail.com>
2021-10-23 15:34:17 +0200

    Remove unnecessary final from private method

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet02/homework/InternalHomeworkHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet02/homework/InternalHomeworkHamsterGame.java
@@ -15,7 +15,7 @@ public abstract class InternalHomeworkHamsterGame extends InspectableSimpleHamst
 		testAllGrainsInCave();
 	}
 
-	private final void testAllGrainsInCave() {
+	private void testAllGrainsInCave() {
 		paule.write("Test:");
 		Territory territory = game.getTerritory();
 		if (territory.getNumberOfGrainsAt(new Location(6, 1)) == territory.getTotalGrainCount() && paule.mouthEmpty()) {
